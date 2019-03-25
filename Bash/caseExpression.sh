9039310cee37783f0ef37a358fc13570138ee5dd
Treelovah <mwlivingston@mltechi.com>
2019-03-24 20:59:19 -0600

    most recent updates

--- Bash/caseExpression.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Script to evaluate grades
-# Usage: grade.sh student grade
-# Author: @theurbanpenguin
-# Date: 1/1/1971
-if [ ! $# -eq2 ] ; then
-	echo "You must provide <student><grade>"
-	exit 2
-fi

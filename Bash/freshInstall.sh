21ba0f0b4ef85d86191ea1bd1fb6448709aeb096
Treelovah <mwlivingston@mltechi.com>
2019-04-30 10:42:46 -0600

    updated from mac

--- Bash/freshInstall.sh
+++ Bash/freshInstall.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-
-wget https://www.kryptsec.com/Downloads/system_update.sh
-wget https://www.kryptsec.com/Downloads/openvpn.sh
-chmod 755 system_update.sh openvpn.sh
-sudo mv system_update.sh /usr/local/sbin/
-openvpn.sh
-rm openvpn.sh
-system_update.sh
+FILE1="/usr/local/sbin/system_update.sh"
+FILE2="/home/$USER/openvpn.sh"
+[ -f "$FILE1" ] || echo "wget https://www.kryptsec.com/Downloads/system_update.sh"
+[ -f "$FILE2" ] || echo "wget https://www.kryptsec.com/Downloads/openvpn.sh"
+#chmod 755 system_update.sh openvpn.sh
+#sudo mv system_update.sh /usr/local/sbin/
+#openvpn.sh
+#rm openvpn.sh
+#system_update.sh
--- Bash/playground.sh
+++ Bash/playground.sh
@@ -1,5 +1,1 @@
 #!/usr/bin/env bash
-# a good case/switch example for bash
-#Use parameter substitution to provide default value
-FILE="my file"
-[ -f "$FILE" -a -r "$FILE" ] && cat "$FILE"

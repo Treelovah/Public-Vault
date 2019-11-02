7f47af9da8e34c9a655dcf351d17a4fc188b3fa0
Treelovah <mltechi@pm.me>
2019-11-02 11:44:43 -0600

    small tweaks to incomplete files

---
+++ Python_CS-150/Eight_Ball.py
@@ -0,0 +1,16 @@
+import random
+# /**
+#  * 
+#  * Basic magic 8 ball, with an equal number of options in each category. Teaches 
+#  * basic if / else statements.
+#  * 
+#  * 
+#  * @author Marshall Livingston <br>
+#  *         mltechi@rams.colostate.edu <br>
+#  *         Computer Science Department <br>
+#  *         Colorado State University
+#  * @version 201990
+#  */
+
+class Eight_Ball:
+    pass

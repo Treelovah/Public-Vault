563ee9f2d3ab37769e3645f2a8f38d1fa8fee3b7
Treelovah <mltechi@pm.me>
2020-02-27 09:27:21 -0700

    updated debian.sh script and updated pointers.cpp

--- CPP/Pointers/pointers.cpp
+++ CPP/Pointers/pointers.cpp
@@ -1,13 +1,23 @@
+/*
+Now let's put pointers to use. This program uses a function named double_it,
+which doubles a variable passed to it or, rather, doubles a variable whose address is passed to it.
+
+*/
 #include <iostream>
-// fuck these are cool:
-// #define area(r) (3.14*r*r)
 using namespace std;
-#include <math.h>
-int main(int argc, char const *argv[])
-{
-    int x = 5;
-    int a = *&x;
-    a = std::pow((double)a, 2.0);
-    cout << a << " " << x << '\n';
+
+void double_it(int *p);
+
+int main() {
+    int a = 5, b = 6;
+    cout << "Val. of 'a' before doubling : " << a << endl;
+    cout << "Val. of 'b' before doubling : " << b << endl;
+    double_it(&a);
+    double_it(&b);
+    cout << "Val. of 'a' after doubling : " << a << endl;
+    cout << "Val. of 'b' after doubling : " << b << endl;
     return 0;
+}
+void double_it(int *p) {
+    *p = *p * 2;
 }

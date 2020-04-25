ffbbb794c28ea96e570de76418a7bc6dede92796
Treelovah <mltechi@pm.me>
2020-04-24 22:25:21 -0600

    Organizing everything

--- PHP/cookies.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$name = "SomeName";
-$value = 100;
-$expire = time() + (60*60*24*7);
-
-setcookie($name, $value, $expire);
--- PHP/for_each.php
+++
@@ -1,5 +0,0 @@
-<?php
-$numbers = array(1,2,3,4,5,6,7,8,9);
-foreach($numbers as $number) {
-    echo $number;
-}
